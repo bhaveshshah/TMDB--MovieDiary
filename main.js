a956b5b9fa4b75eaa356a4052a1c5c27867288ec
« Camille Rahout» <camille.rahout@gmail.com>
2026-01-20 14:01:29 +0100

    first commit of the movie container

--- main.js
+++ main.js
@@ -1,1 +1,106 @@
 console.log("Main JS file loaded successfully.");
+
+//Code for the movie blocks will go here
+///First block: popular movies
+
+const popularMovieContainer = document.getElementById('popular-movies-card-container');
+
+////Function to fetch the popular movies popularity rate
+
+async function fetchPopularMovies() {
+    try {
+        const res = await fetch (`https://api.themoviedb.org/3/movie/popular?api_key=cc509547e8cb8d5e318b618432577237`);
+        const movie = await res.json();
+        return movie.results;
+    } catch (error) {
+        console.error('Error fetching movie data:', error);
+    }
+}
+////function to create the movie card
+
+function createPopularMovieCard (movie){
+    const popularMovieCard = document.createElement("div");
+    popularMovieCard.classList.add('bg-white', 'rounded-lg', 'shadow-md', 'p-4', 'flex', 'flex-col', 'item-center', 'text-center');
+    
+    const popularMovieImage = document.createElement('img');
+    popularMovieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    popularMovieImage.alt = movie.title;
+    popularMovieImage.classList.add("mb-2");
+    
+    const popularMovieName = document.createElement("h3");
+    popularMovieName.textContent = movie.title;
+    popularMovieName.classList.add('text-xl', 'font-bold')
+
+    const popularMovieInfo = document.createElement("p");
+    popularMovieInfo.textContent = `Popularity rate: ${movie.popularity}, Synopsis: ${movie.overview}`;
+    popularMovieInfo.classList.add('text-gray-600');
+
+    popularMovieCard.appendChild(popularMovieImage);
+    popularMovieCard.appendChild(popularMovieName);
+    popularMovieCard.appendChild(popularMovieInfo);
+    popularMovieContainer.appendChild(popularMovieCard);
+    };
+
+////function to display the movies
+
+async function displayPopularMovies() {
+    const popularMovies = await fetchPopularMovies();
+    if (!popularMovies) return;
+    popularMovies.forEach(popularMovie => {
+        createPopularMovieCard(popularMovie);
+    });
+};
+
+displayPopularMovies();    
+
+///Second block: latest movies
+
+const latestMovieContainer = document.getElementById('latest-release-card-container');
+
+////Function to fetch the latest movies
+
+async function fetchLatestMovies() {
+    try {
+        const res = await fetch (`https://api.themoviedb.org/3/discover/movie?api_key=cc509547e8cb8d5e318b618432577237&include_video=false&primary_release_date.gte=2025-10-01&primary_release_date.lte=2026-01-20`);
+        const latestMovie = await res.json();
+        return latestMovie.results;
+    } catch (error) {
+        console.error('Error fetching movie data:', error);
+    }
+}
+////function to create the movie card
+
+function createLatestMovieCard (movie){
+    const latestMovieCard = document.createElement("div");
+    latestMovieCard.classList.add('bg-white', 'rounded-lg', 'shadow-md', 'p-4', 'flex', 'flex-col', 'item-center', 'text-center');
+    
+    const latestMovieImage = document.createElement('img');
+    latestMovieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    latestMovieImage.alt = movie.title;
+    latestMovieImage.classList.add("mb-2");
+    
+    const latestMovieName = document.createElement("h3");
+    latestMovieName.textContent = movie.title;
+    latestMovieName.classList.add('text-xl', 'font-bold')
+
+    const latestMovieInfo = document.createElement("p");
+    latestMovieInfo.textContent = `Popularity rate: ${movie.popularity}, Synopsis: ${movie.overview}`;
+    latestMovieInfo.classList.add('text-gray-600');
+
+    latestMovieCard.appendChild(latestMovieImage);
+    latestMovieCard.appendChild(latestMovieName);
+    latestMovieCard.appendChild(latestMovieInfo);
+    latestMovieContainer.appendChild(latestMovieCard);
+    };
+
+////function to display the movies
+
+async function displayLatestMovies() {
+    const latestMovies = await fetchLatestMovies();
+    if (!latestMovies) return;
+    latestMovies.forEach(latestMovie => {
+        createLatestMovieCard(latestMovie);
+    });
+};
+
+displayLatestMovies();    
